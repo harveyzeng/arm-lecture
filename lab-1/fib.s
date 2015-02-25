88247da369f01cc17aa0b935e621ea269aead96e
harvey <ff6416@hotmail.com>
2015-02-25 21:00:57 +0800

    no recursive

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -18,32 +18,18 @@ fibonacci:
         cmp r4, #1        // Compare r4 to 1
         beq .L4        // if (r4 == 1) goto .L4
 
-  	//add r0, r4, #4294967295 
-          sub r0,r4,#1
- // r0 = r4 + 4294967295 (or #0xFFFFFFFF)
- 	bl fibonacci          // goto fibonacci @PC relative address
+ 	subs r4,r4,#1 //r4=r4-1 
+	mov  r6,#1
+        mov  r7,#0     
+l1:     
+  	subs r4,r4,#1 //r4=r4-1 
+	adds r0,r6,r7 //r0=r6+r7
+	mov  r7,r6    //r7=r6
+	mov  r6,r0    //r6=r0
+        cmp  r4,#0    //if(r4!=0) go to l1
+	bne  l1
+
 	
- 	 mov r5, r0        // r5 = r0
- 	 sub r0, r4, #2    // r0 = r4 - 2
- 	 bl fibonacci        // goto fibonacci @PC relative address
-
- 	 adds r0, r5, r0
-
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
