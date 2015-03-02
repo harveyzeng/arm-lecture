88a2d48ec69734effccf2ea24f24a19b28eef515
harvey <harvey@hotmail.com>
2015-03-02 12:15:54 +0800

    scanf need if else

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -6,9 +6,15 @@ int main(int argc, char **argv)
 {
   int number=0;
   int result=0;
-
-  scanf("%d",&number);
-  result = fibonacci(number);   
+ if (scanf("%d", &number) == 1) {
+    result = fibonacci(number);   
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+    } else {
+        printf("failed to read integer.\n");
+    }
+ // scanf("%d",&number);
+ // result = fibonacci(number);   
+//  printf("The fibonacci sequence at %d is: %d\n", number, result);
+return 1;
 }
 
